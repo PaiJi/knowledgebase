4e62afb212da36356a2da2b86613127983cb7afa
JiPai <github@jipai.moe>
2023-04-01 22:00:30 +0800

    init commit

---
+++ docs/system-tricks/MacOS/disabled-ds-store.md
@@ -0,0 +1,3 @@
+
+#### Sources
+https://support.studionetworksolutions.com/hc/en-us/articles/360000949866-Prevent-creation-of-DS-Store-files-on-network-shares
---
+++ docusaurus.config.js
@@ -0,0 +1,104 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "JiPai Knowledgebase",
+  tagline: "Dinosaurs are cool",
+  url: "https://knowledgebase.jipai.moe",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "JiPai's knowledgeBase",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "知识库",
+          },
+          // {to: '/blog', label: '笔记', position: 'left'},
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "More",
+            items: [
+              {
+                label: "首页",
+                href: "https://www.jipai.moe",
+              },
+              {
+                label: "Blog",
+                href: "https://blog.jipai.moe",
+              },
+              { label: "Projects", href: "https://www.jipai.moe/projects" },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} [JiPai Store], Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
